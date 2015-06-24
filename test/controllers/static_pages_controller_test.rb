40da1ca7365f0257f1c7e693a903292ce99268bd
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-06-24 16:14:08 +0200

    Add about page and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  @@static_title_string = " | Ruby on Rails Tutorial Sample App"
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home"+@@static_title_string
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help"+@@static_title_string
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About"+@@static_title_string
+  end
 end
