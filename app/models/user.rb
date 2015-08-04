064dcf81383b67dccc806526b5af9a100859e075
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-08-04 15:57:09 +0200

    some improvement

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,9 @@ class User < ActiveRecord::Base
   
   # active account
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true,
+                   activated_at: Time.zone.now,
+                   activation_digest: '')
   end
 
   # send activation mail
@@ -54,8 +55,8 @@ class User < ActiveRecord::Base
   # 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # 
