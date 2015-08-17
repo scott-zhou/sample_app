fcf3af1829690be0c92827165e3aff2eeb0bb26c
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-08-17 13:30:25 +0200

    Change host URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'murmuring-sierra-2494.herokuapp.com'
+  host = 'ror-sample-app-sc.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
